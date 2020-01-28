d928581161ea59cda3d8a3d95200a6b6303f9d08
H.Chen <chenhj2000@163.com>
2020-01-28 17:14:37 +0800

    Start Project

---
+++ trail.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+if __name__ == '__main__':
+    array = np.array([[1, 1], [1, 1]])
+    print(array.ndim)
+    print(type(array))
+    # if isinstance(array, 'numpy'):
+    #     print(1)
+    tex_code = "\\begin{table}[htbp]\n\t\\centering\n\t\\begin{tabular}{cc}"
+    print(tex_code)
+
