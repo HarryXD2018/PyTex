c1100c2174a43e37ba685acafee58935667d9ed4
H.Chen <chenhj2000@163.com>
2020-04-23 16:48:14 +0800

    Change example

--- Example/example.py
+++ Example/example.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     tex = PyTex()
     # This mode will print the result
 
-    # df = pd.DataFrame(np.arange(12).reshape((3, 4)), index=['one', 'two', 'thr'], columns=list('abcd'))
-    # tex.raw_tex(df)
+    df = pd.DataFrame(np.arange(12).reshape((3, 4)), index=['one', 'two', 'thr'], columns=list('abcd'))
+    tex.raw_tex(df)
 
     # table = [['Team1', 'Tom', 'Jack'], ['Team2', 'Lucy', 'Anne'], ['Team3', 'Joe']]
     # tex.raw_tex(table)
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # city = ['New York', 'Shanghai', 'London']
     # tex.make_table_by_row(nation, city, vline=False)
     # tex.make_table_by_column(nation, city)
-    tex.code_insert("example.py", high_light=True, language='py')
+    # tex.code_insert("example.py", high_light=True, language='py')
 
     # tex_board = PyTex(True)  # If copy_to_clipboard is True, it won't print but copy to clipboard instead.
     # matrix = [[0, 1], [1, 0]]
